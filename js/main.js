0241196df3a7d65e76cf0c2300a845eb46b53c28
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-03-22 13:39:57 +0000

    Register Service Worker file in our application

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 // Make sure sw are supported.
+if ('serviceWorker' in navigator) {
+	// We want to register service worker when the window loads.
+	window.addEventListener('load', () => {
+		navigator.serviceWorker
+			.register('../sw_cached_pages.js')
+			.then(() => console.log('Service worker is registered.'))
+			.catch(err => console.log(`Service Worker failed to register: ${err}`));
+	});
+}
